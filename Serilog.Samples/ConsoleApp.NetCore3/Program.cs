3d8653c77a959201333f8db1670e46e847ccfeb3
Daniel Schroeder <deadlydog@hotmail.com>
2019-10-03 22:21:20 -0600

    refactor: Remove unused code

--- Serilog.Samples/ConsoleApp.NetCore3/Program.cs
+++ Serilog.Samples/ConsoleApp.NetCore3/Program.cs
@@ -44,7 +44,7 @@ namespace ConsoleApp.NetCore3
 			Log.Debug("Here's a Debug message. Only Public Properties (not fields) are shown on structured data. Structured data: {@sampleData}. Simple data: {simpleData}.", structuredData, simpleData);
 			Log.Information(new Exception("Exceptions can be put on all log levels"), "Here's an Info message.");
 			Log.Warning("Here's a Warning message.");
-			Log.Error(new Exception("This is an exception."), "Here's an Error message.", structuredData, simpleData);
+			Log.Error(new Exception("This is an exception."), "Here's an Error message.");
 			Log.Fatal("Here's a Fatal message.");
 		}
 	}
