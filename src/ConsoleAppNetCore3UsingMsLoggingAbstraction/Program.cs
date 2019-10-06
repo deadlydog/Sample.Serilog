5c21362747494e7d2f381f8f52f0752e7d1e1ab9
Daniel Schroeder <deadlydog@hotmail.com>
2019-10-06 11:56:26 -0600

    Add new vanilla console app project

---
+++ src/ConsoleAppNetCore3UsingMsLoggingAbstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppNetCore3UsingMsLoggingAbstraction
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
